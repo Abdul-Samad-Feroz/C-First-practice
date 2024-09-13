5b3eb4e469dd0e02e4a5d8a95c7a072f0bc085b5
Abdul Samad <abdulsamadferoz786@gmail.com>
2024-09-13 21:58:21 +0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string sum = "10";
+    //cout<< "My name is: " + sum <<endl;
+
+  //  string names [10] = {"anas" , "samad" , "bilal" , "ibrahim"};
+
+//    cout<< names[3] << endl;
+
+    cout<< "Enter your name" << endl;
+
+    string name;
+     cin >> name ;
+     cout << name <<endl;
+
+    int n = 5;
+
+    for(int i=0; i<n; i++){
+        cout<< i << endl;
+    }
+
+    return 0;
+}
